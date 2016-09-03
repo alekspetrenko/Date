a5df390ab251f533a55f6b1d6e95fd08f5810a00
Aleksandr Petrenko <petrenko.aleksandr025@gmail.com>
2016-09-03 23:19:04 +0300

    outputing today's date

---
+++ src/HelloDate.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+
+public class HelloDate {
+
+	public static void main(String[] args) {
+		Date currDate = new Date();
+		System.out.println("Сегодня: " + currDate);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
